49a67c8ac25a677113de49ff389df0d5f7f64334
Arpit0408 <Paliwalarpit180@gmail.com>
2023-12-05 15:11:09 +0530

    add new files

---
+++ Color.js
@@ -0,0 +1,25 @@
+const Buttons=document.querySelectorAll('.button')
+// console.log(Buttons);
+const body =document.querySelector('body')
+Buttons.forEach(function(button){
+    // console.log(button);
+
+    button.addEventListener('click' , function(e){
+// console.log(e);
+// console.log(e.target);
+if(e.target.id === 'grey'){
+    body.style.backgroundColor = e.target.id
+}
+if(e.target.id === 'white'){
+    body.style.backgroundColor = e.target.id
+}if(e.target.id === 'blue'){
+    body.style.backgroundColor = e.target.id
+}if(e.target.id === 'Crimson'){
+    body.style.backgroundColor = e.target.id
+}if(e.target.id === 'Pink'){
+    body.style.backgroundColor = e.target.id
+}if(e.target.id === 'yellow'){
+    body.style.backgroundColor = e.target.id
+}
+    })
+})
